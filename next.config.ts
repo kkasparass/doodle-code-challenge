df45ca52515fc4bf53bd562590b68f836a32a855
kkasparass <kkasparass@gmail.com>
2026-02-24 07:52:30 +0100

    Created new NextJs app

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+  turbopack: {},
+};
+
+export default nextConfig;
