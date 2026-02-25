0e59e0b3e1e3eb3b885b154db34997e7c52dbeea
kkasparass <kkasparass@gmail.com>
2026-02-25 12:27:52 +0100

    Fix wring env variable

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTHORIZATION_HEADER}`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`,
   },
 });
 
